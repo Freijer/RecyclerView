749e15bcd0f0f752e783bd328a48c833c466a4ff
Freijer <dm.polikarpov@gmail.com>
2021-02-15 11:46:53 +0300

    15/10/21

--- app/src/main/java/freijer/app/one/MainActivity.java
+++ app/src/main/java/freijer/app/one/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,56 +16,24 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
+    static int counter_plus = 0;
+    static int plusLenght = 0;
+        static int counter_stay = 0;
+        static int stayLenght = 0;
+
+
+
+
     protected int plusLength; //+1 буква в слове
     private int stayLength; //слова той же длинны подряд раз
     private int minusLength; //-1 буква в слове
     protected  ArrayList<String> Alfas = new ArrayList<>(); //вхоящий прогресс
-    protected  ArrayList<String> Buffer = new ArrayList<>(); //вхоящий прогресс
-
-    protected int count_word_plus;
-    protected int count_word_stay;
-    protected int count_word_minus;
 
 
 
-    protected int simbol_lenght;
-    protected int count_simbol_up;
-
-    public int getSimbol_lenght() {
-        return simbol_lenght;
-    }
-    public void setSimbol_lenght(int simbol_lenght) {
-        this.simbol_lenght = simbol_lenght;
-    }
-    public int getCount_simbol_up() {
-        return count_simbol_up;
-    }
-    public void setCount_simbol_up(int count_simbol_up) {
-        this.count_simbol_up = count_simbol_up;
-    }
-
-
-
-    public int getCount_word_plus() {
-        return count_word_plus;
-    }
-    public void setCount_word_plus(int count_word_plus) {
-        this.count_word_plus = count_word_plus;
-    }
-    public int getCount_word_stay() {
-        return count_word_stay;
-    }
-    public void setCount_word_stay(int count_word_stay) {
-        this.count_word_stay = count_word_stay;
-    }
-    public int getCount_word_minus() {
-        return count_word_minus;
-    }
-    public void setCount_word_minus(int count_word_minus) {
-        this.count_word_minus = count_word_minus;
-    }
-
-    TextView count_plus, count_stay, count_minus, max_plus;
+    TextView count_plus, max_plus, max_stay;
+    Button pressMe;
+    EditText inText_plus, inText_stay;
 
 
     @SuppressLint("SetTextI18n")
@@ -72,10 +43,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-//        Alfas.add("т");
-//        Alfas.add("то");
-//        Alfas.add("тор");
-//        Alfas.add("торт");
+        Alfas.add("т");
+        Alfas.add("то");
+        Alfas.add("тор");
+        Alfas.add("торт");
         Alfas.add("торти");
         Alfas.add("тортик");
         Alfas.add("тортики");
@@ -106,33 +77,52 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("test_-1", "уменьшение на 1 букву: "+minusLength);
 
 
+
         count_plus = findViewById(R.id.count_plus);
-        count_stay = findViewById(R.id.count_stay);
-        count_minus = findViewById(R.id.count_minus);
         max_plus = findViewById(R.id.max_plus);
+        max_stay = findViewById(R.id.max_stay);
+
+        pressMe = findViewById(R.id.pressMe);
+        inText_plus = findViewById(R.id.inText_plus);
+        inText_stay = findViewById(R.id.inText_stay);
 
 
-        setCount_word_plus(plusLength);
+        max_plus.setText(""+counter_plus);
+        max_stay.setText(""+counter_stay);
 
-
-        counterUo(plusLength);
     }
 
-//    protected void counterUo(int income){
-//        if (income >2) {
-//            for(int i=0; i<2; i++) {
-//                Buffer.add("" + income);
-//            }
-//        }
-//        Log.d("test_-po", "размер: " + (Buffer.size()));
-//    }
 
-    protected void counterUo(int income){
-        if (income >2) {
-            count_simbol_up++;
+    public void kliker(View v){
+        int noteYeat_stay = 5;
+        String take_plus = inText_plus.getText().toString();
+        String take_stay = inText_stay.getText().toString();
+        int in = Integer.parseInt(take_plus);
+        add_plus(in);
+        max_plus.setText(""+counter_plus);
+            int in_stay = Integer.parseInt(take_stay);
+            add_stay(in_stay, noteYeat_stay);
+            max_stay.setText(""+counter_stay);
+
+
+    }
+
+
+    private void add_plus (int input) {
+        if (input > 2) {
+            counter_plus++;
         }
-        Log.d("test_-po", "размер: " + (count_simbol_up));
+        plusLenght = input;
     }
+
+    private void add_stay (int input, int param) {
+        if (input == param) {
+            counter_stay++;
+        }
+        stayLenght = input;
+    }
+
+
 
 
     public int CountCorrectSeqLen(List<String> data) {
@@ -143,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 plusLength = Math.max(plusLength, currentLength);
                 currentLength = 1;
-
             }
         }
         plusLength = Math.max(plusLength, currentLength);
@@ -175,6 +164,8 @@ public class MainActivity extends AppCompatActivity {
         minusLength = Math.max(minusLength, currentLength);
         return minusLength;
     }// слов одинаковой длинны уменьшение
+
+
 
 
 
